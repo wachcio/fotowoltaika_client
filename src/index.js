f67c8cee9d54cb2d387263d0180fce0ae0f79755
wachcio <wachcio@poczta.fm>
2021-08-08 12:58:38 +0200

    Basic

--- src/index.js
+++ src/index.js
@@ -1,14 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+import './SCSS/index.scss';
 import App from './App';
+import Spiner from './components/spinner/spinner';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+    <React.StrictMode>
+        <Spiner />
+        <App />
+    </React.StrictMode>,
+    document.getElementById('root'),
 );
 
 // If you want to start measuring performance in your app, pass a function
---
+++ src/store/storeProvider.js
@@ -0,0 +1,65 @@
+import React, { createContext } from 'react';
+import { useStateWithLabel } from '../helpers/helpers';
+
+import Message from '../components/message/message';
+
+// const LOADS_DATA = [];
+export const StoreContext = createContext(null);
+
+const StoreProvider = (props) => {
+    // Load
+
+    const [loadData, setLoadData] = useStateWithLabel('loadData', null);
+    const [takeLoad, setTakeLoad] = useStateWithLabel('takeLoad', false);
+
+    // Menu
+    const [isOpenMenu, setIsOpenMenu] = useStateWithLabel('isOpenMenu', false);
+
+    // User data
+    const [userData, setUserData] = useStateWithLabel('userData', {
+        username: null,
+        email: null,
+        isLogged: false,
+        token: null,
+    });
+
+    // Message
+    const [isVisibleMessage, setIsVisibleMessage] = useStateWithLabel('isVisibleMessage', false);
+    const [messageText, setMessageText] = useStateWithLabel('messageText', '');
+    const [isMessageAlert, setIsMessageAlert] = useStateWithLabel('isMessageAlert', false);
+
+    // Router links
+    const routerLinks = [{ name: 'start', path: '/', exact: true }];
+
+    const showMessage = (text, isAlert) => {
+        setMessageText(text);
+        setIsMessageAlert(isAlert);
+        setIsVisibleMessage(true);
+
+        setTimeout(() => {
+            setIsVisibleMessage(false);
+        }, 5000);
+    };
+
+    return (
+        <StoreContext.Provider
+            value={{
+                loadData,
+                setLoadData,
+                takeLoad,
+                setTakeLoad,
+                userData,
+                setUserData,
+                showMessage,
+                routerLinks,
+                setIsOpenMenu,
+                isOpenMenu,
+            }}
+        >
+            {isVisibleMessage ? <Message message={messageText} alert={isMessageAlert} /> : ''}
+            {props.children}
+        </StoreContext.Provider>
+    );
+};
+
+export default StoreProvider;
